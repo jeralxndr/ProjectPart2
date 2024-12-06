fd82b88090df10521561d37f2faf8f8e7f20d8bb
jeralxndr <jeremy.alexander4@ontariotechu.net>
2024-12-05 23:56:58 -0500

    render

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('./server/config/app');
+var app = require('./Server/config/app');
 var debug = require('debug')('infrproject:server');
 var http = require('http');
 
